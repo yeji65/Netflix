057fc5c0814b2892d6d5867e2a7e077363c61d53
yeji65 <94824407+yeji65@users.noreply.github.com>
2022-07-18 13:24:00 +0900

    css수정

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -5,6 +5,9 @@ import { useEffect } from 'react'
 import { useDispatch,useSelector } from 'react-redux'
 import { MovieAction } from '../redux/actions/MovieAction'
 import { Badge } from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Button from 'react-bootstrap/Button';
+//import { fa-solid fa-star } from '@fortawesome/free-regular-svg-icons';
 
 const MovieDetail = () => {
   const { id } = useParams();
@@ -28,25 +31,47 @@ const MovieDetail = () => {
 
   return (
     <Container>
-      <Row>
-        <Col className="">
+      <Row className='movie-detail'>
+        <Col className="movie-img" >
           <img src={"https://www.themoviedb.org/t/p/w300_and_h450_bestv2"+movie?.poster_path} />
         </Col>
         <Col>
+          {/* <div>{ movie.genres.map((id)=>(<Badge bg="danger">{id}</Badge>))}</div> */}
           <h1>{movie?.title}</h1>
-          {/* <div>{movie?.genre_ids.map((id)=>(<Badge bg="danger">{genreList.find((item)=>item.id == id).name}</Badge>))}</div> */}
-          <div>{movie?.overview}</div>
+          <div className='movie'>
+            <span className="movie-info">{movie?.vote_average}</span>
+            {/* <FontAwesomeIcon icon="fa-solid fa-star"/> */}
+            <span className="movie-info">{movie?.popularity}</span>
+            <span className="movie-adult">{movie && movie.adult?"청불":"Under 18"}</span>
+          </div>
+          <div className="movie-overview" >{movie?.overview}</div>
+          <div >
+            <div className='movie-low'><Badge bg="danger" className='movie-badge'>budget</Badge> ${movie?.budget}</div>
+            <div className='movie-low'><Badge bg="danger" className='movie-badge'>revenue</Badge> ${movie?.revenue}</div>
+            <div className='movie-low'><Badge bg="danger" className='movie-badge'>release date</Badge> {movie?.release_date}</div>
+            <div className='movie-low'><Badge bg="danger" className='movie-badge'>time</Badge> {movie?.runtime}</div>
+          </div>
         </Col>
-          <div>
-            <h1>{review?.results[0].author}</h1>
-            <div>{review?.results[0].content}</div>
-            <h1>{review?.results[1].author}</h1>
-            <div>{review?.results[1].content}</div>
-            <h1>{review?.results[2].author}</h1>
-            <div>{review?.results[2].content}</div>
+          <h1 className='movie-low'>
+          <Button variant="danger">REVIEW</Button>{' '}
+          <Button variant="light">RELATED MOVIES</Button>{' '}
+          </h1>
+          <div className="movie-review3" >  
+            <div className="movie-review2">
+              <h1 >{review && review?.results[0].author}</h1>
+              <div className="movie-reviewline">{review && review?.results[0].content}</div>
+            </div>
+            <div className="movie-review2">
+              <h1>{review && review?.results[1].author}</h1>
+              <div className="movie-reviewline">{review && review?.results[1].content}</div>
+            </div>
+            <div className="movie-review2">
+              <h1>{review && review?.results[2].author}</h1>
+              <div>{review && review?.results[2].content}</div>
+            </div>
           </div>
           <div>
-            <img src={"https://www.themoviedb.org/t/p/w300_and_h450_bestv2"+recommendation?.results[1].poster_path}/>
+            <img src={"https://www.themoviedb.org/t/p/w300_and_h450_bestv2"+recommendation?.results[0].poster_path}/>
           </div>
       </Row>
     </Container>
